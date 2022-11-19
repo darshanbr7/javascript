871ee201a8274d45071c6eaac4d5956b1bb52ee7
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2022-11-19 22:20:58 +0530

    kk

---
+++ arr.js
@@ -0,0 +1,11 @@
+// some array methods
+let a=[]
+
+// push method
+console.log(a.push("10"))
+console.log(a)
+console.log(a.push("darshan",1010,"muni","chirag"))
+console.log(a)
+// pop method
+console.log(a.pop())
+console.log(a)
---
+++ class.js
@@ -0,0 +1,12 @@
+ class employee{
+    constructor(id,name){
+        this.id=id
+        this.name=name
+    }
+    
+}
+  let r1=new employee(101,"darshan")
+  console.log("Id is",r1.id)
+  console.log( "Name is",r1.name)
+  console.log(r1.id+r1.name)
+  console.log(r1.id-r1.name)
