c366c92d81339eb3529fd7b433446390127d671d
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2022-11-27 19:34:32 +0530

    problem

---
+++ problems/1.js
@@ -0,0 +1,12 @@
+// find a duplicate letter in a given string
+let dup=(str)=>{
+    let arr=[]
+    for(i=0;i<str.length;i++){
+       if(str[i]==){
+        console.log(i)
+        
+       }
+    }
+
+}
+ dup(["hello","hello", "hii"])
