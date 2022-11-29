bfb8062af2e4e6f34731683f80f6c5f3020ad016
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2022-11-29 11:13:39 +0530

    hoisting

---
+++ hoisting.js
@@ -0,0 +1,7 @@
+console.log(hois(1,3))
+
+function hois(a,b){
+     return a+b
+}
+
+/* hoisting is the one of the feature it access the variables before the declaration*/
---
+++ this.js
@@ -0,0 +1,7 @@
+let address={
+    from:"mandya",
+    to:"Benglore"
+}
+console.log("I am from "+address.from+" i am going to "+address.to)
+// 
+console.log(this.address.from)
