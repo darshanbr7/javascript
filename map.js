ca08be126a6ef2b01fee2e8831f947970e750321
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2022-11-30 11:25:19 +0530

    MAP

---
+++ map.js
@@ -0,0 +1,13 @@
+let Data=[{"id":1,"name":"Hewie","email":"hgillham0@businessweek.com","gender":"Male"},
+{"id":18,"name":"Constancy","email":"cbraffingtonh@webnode.com","gender":"Female"},
+{"id":19,"name":"Erastus","email":"eallsworthi@soundcloud.com","gender":"Male"},
+{"id":20,"name":"Norry","email":"ncesconij@samsung.com","gender":"Male"}]
+ Data.map((kk)=>{
+console.log(kk)
+ })
+ /* map method recive the three arguments 
+                map(1,2,3)
+
+                            1-- it indecates the array elements
+                            2-- it indicates the array elements index
+                            3-- it indicates the whole array  */
