c2ae9c185dfbb1ef0b416651b44deccb5f019079
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2023-02-16 19:11:47 +0530

    "javascript"

---
+++ practice/Boolean.js
@@ -0,0 +1,4 @@
+let a=true;
+console.log(typeof(a));
+console.log(typeof(a.toString())) // converts to the String
+console.log(a.length)
---
+++ practice/S2.js
@@ -0,0 +1,7 @@
+let  i=0;
+let sum=0;
+for(i=1;i<=10;++i){
+    sum=sum+i*2;
+
+}
+console.log(sum)
