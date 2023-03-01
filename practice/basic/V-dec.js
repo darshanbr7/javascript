e002fcb8634fb4b87f9c2cf1e0eccb0ca253434e
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2023-03-01 11:16:31 +0530

    java script

---
+++ practice/basic/V-dec.js
@@ -0,0 +1,19 @@
+// variable declaration using let var and const
+// var keyword is using from the begining 
+// let and const introduced i ES6 --in 2015
+
+/* variabe decalaration usig var
+var a;// declaration
+console.log(a)
+a=10;// assigning
+console.log(a)
+a=20; // reassigning
+console.log(a);
+var a=30; // re declaration
+console.log(a);
+*/
+
+// varoiable declaration using let keyword
+ 
+
+
