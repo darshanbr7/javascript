acc13ddb231a84f0e032e45fb881f83664bf29d7
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2023-03-01 12:07:24 +0530

    variables

--- practice/basic/V-dec.js
+++ practice/basic/V-dec.js
@@ -13,7 +13,27 @@ var a=30; // re declaration
 console.log(a);
 */
 
-// varoiable declaration using let keyword
- 
+// // varoiable declaration using let keyword
+//  let b;// declaration
+//  b=10;// assigning
+//  b=20;// reAssigning
+//  let b=40;// re declaration
+// // when using let keyword we con't do reclaration
+
+/*variable declaration using const keyword
+const c;// declaration
+c=10;// assigning
+c=19; // re assigning
+let c=13; // re declaration
+
+
+// in const  it not suport the re declaration reAssigning
+*/
+
+
+
+
+
+
 
 
