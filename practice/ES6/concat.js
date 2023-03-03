0f60f2f2bb3746887d937a4dff49e04acec869b6
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2023-03-03 18:42:03 +0530

    array methods

---
+++ practice/ES6/concat.js
@@ -0,0 +1,6 @@
+let a=[1,2,3,4];
+let b=[5,6,7];
+let c=a.concat(b)
+
+console.log(a.concat(b))
+// concat method is used to concating two Arrays
---
+++ practice/ES6/every.js
@@ -0,0 +1,7 @@
+let a=[4,5,6,7,8]
+a.every((ele)=>{
+console.log(ele>=4)
+})
+// every() --> this method used to check the condition 
+/* this method return true  -- when all condition are true
+ even one is false it return "false" */
